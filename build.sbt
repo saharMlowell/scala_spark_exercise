3cf8dadf8bb77220cb4b224b8053cfaae59bce6c
Sahar Lowell <slowell@rubiconproject.com>
2017-08-29 16:16:04 -0700

    First commit

---
+++ build.sbt
@@ -0,0 +1,2 @@
+name := "scala-spark-exercise"
+scalaVersion := "2.11.8"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,4 @@
+object Main {
+  // let's do a bare minimum, working example
+  def main(args: Array[String]): Unit = println("hello world!")
+}
