1b0acd86678369e0fb23c9f8a77205a92a501ff0
Sahar Lowell <slowell@rubiconproject.com>
2017-08-31 14:20:00 -0700

    adding parquet file.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 https://ntoggle.atlassian.net/browse/PLAT-1831<br>
 1- having simple people.json file, name, age and city<br>
 2- load data with spark as dataFrame<br>
-3- filter (spark) on a col, make a query, find ppl greater than some age. //df.filter(age > 21).select('name')<br>
-4- output the result<br>
-5- save to parquet data set<br>
+3- save to parquet data set<br>
+4- load the parquet data set, filter on a col, make a query, find ppl greater than some age.<br>
+5- output the result<br>
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,13 @@ object Exercise extends App{
   import spark.implicits._ // This import is needed to use the $-notation
 
   val df = spark.read.json("src/main/resources/people.json")
-  df.filter($"age" > 21).select("name").show() // Select name of people older than 21
+  df.write.parquet("src/main/resources/people.parquet") // DataFrames can be saved as Parquet files, maintaining the schema information
+
+  // Read in the parquet file created above
+  // Parquet files are self-describing so the schema is preserved
+  // The result of loading a Parquet file is also a DataFrame
+  val parquetDf = spark.read.parquet("src/main/resources/people.parquet")
+
+  parquetDf.filter($"age" > 21).select("name").show() // Select name of people older than 21
 
 }
