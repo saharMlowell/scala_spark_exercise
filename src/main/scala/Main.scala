2dd9ab83a994ce57cde09a3907708ef97eb77216
Sahar Lowell <slowell@rubiconproject.com>
2017-08-31 09:18:27 -0700

    DataFrame Operations

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,8 @@ object Exercise extends App{
   println("Hello World!")
   val spark = SparkSession.builder().master("local").appName("scala_spark_exercise").getOrCreate
   val df = spark.read.json("src/main/resources/people.json")
+  df.select("name").show()
+  df.filter($"age" > 21).show()
   // Displays the content of the DataFrame to stdout
   df.show()
 }
