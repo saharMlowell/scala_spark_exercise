b274d7d0b751f95aadbea4b8130fbea82a281896
Sahar Lowell <slowell@rubiconproject.com>
2017-08-30 17:45:45 -0700

    reading to df

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,4 +5,7 @@ import org.apache.spark.sql.SparkSession
 object Exercise extends App{
   println("Hello World!")
   val spark = SparkSession.builder.appName("scala_spark_exercise").getOrCreate
+  val df = spark.read.json("scala_spark_exercise/src/main/resources/people.json")
+  // Displays the content of the DataFrame to stdout
+  df.show()
 }
